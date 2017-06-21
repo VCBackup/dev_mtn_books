c69de3ae8545e6409e4671232cbbd4458c7280cd
Moore <DMoore@isolvedhcm.com>
2017-06-20 20:55:24 -0600

    Another random comment.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hello');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+//req.body = '{"name": "Rick"}'
+app.use(bodyParser.json());
+//req.body = {name: 'Rick'}
+
+//req = request{headers, body}
+//res = response{}
+//next
+
+var port = 3000;
+app.listen(port, function() {
+    console.log('Listening on port ' + port);
+});
+
